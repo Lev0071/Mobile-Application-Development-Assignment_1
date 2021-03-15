9893110d373ceba432d3016c35a9886984b9dfb7
Yasin Çakar <coder0071@outlook.com>
2021-03-15 19:11:06 +1000

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Yasin Cakar on 15/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
