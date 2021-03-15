b257f136120dfe1347a174195bb1f8bc9fb5db91
Yasin Çakar <coder0071@outlook.com>
2021-03-15 20:12:02 +1000

    •• Add a Background colourr • •• Place background colour and title Z-stack to maintain elements on the same frame (in order to place both elemens on top of each other) •

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(#colorLiteral(red: 0.3490196078, green: 0.3490196078, blue: 0.3490196078, alpha: 1))
+            Text("Pizzas & Kebabs")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0, opacity: 1.0))
+                .padding()
+        }
     }
 }
 
