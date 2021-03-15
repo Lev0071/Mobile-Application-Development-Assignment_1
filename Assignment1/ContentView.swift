44d92abac3dcc5b2c689f2303389738c442df957
Yasin Çakar <coder0071@outlook.com>
2021-03-15 22:59:21 +1000

    •• Organise Milestone 1 elements into constants to give the code more structure and modularity • 	•• >> The goal is to make these properties in the future < •

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 let imageWith = CGFloat(300.0)
 let imageHeight = CGFloat(300.0)
+let imageName = "KebabAndPizza"
+let shortDescription = "A delicous yet simple mix of Proteins, Vegetables and Carbs"
+let story = ""
+
+let title = "Kebabs And Pizzas"
 
 struct ContentView: View {
     var body: some View {
@@ -16,17 +21,17 @@ struct ContentView: View {
             Color(#colorLiteral(red: 0, green: 0.9103174806, blue: 1, alpha: 1))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Image("KebabAndPizza")
+                Image(imageName)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: imageWith
                            , height: imageHeight, alignment: .center)
-                Text("Pizzas & Kebabs")
+                Text(title)
                     .font(.title)
                     .fontWeight(.bold)
                     .foregroundColor(Color(.secondarySystemGroupedBackground))
                     .padding()
-                Text("A delicous yet simple mix of Proteins, Vegetables and Carbs")
+                Text(shortDescription)
                     .font(.caption)
                     .fontWeight(.bold)
                     .foregroundColor(Color(.secondarySystemGroupedBackground))
