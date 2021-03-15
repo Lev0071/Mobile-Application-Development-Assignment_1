afd9965e0be210bb0d70ce0b07191818832c31cd
Yasin Çakar <coder0071@outlook.com>
2021-03-15 20:44:10 +1000

    •• Add image asset for milestone 1 • 	• >> Make image resizable < • 	• >> Put an aspect ratio to not skew the image < • 	• >> Make image resizable < • 	• >> Define a height and width in pixels using the frame modifier < • 	• >> The height and weight was declared as a constant to make modifying and testing easier < •

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -7,16 +7,26 @@
 
 import SwiftUI
 
+let imageWith = CGFloat(300.0)
+let imageHeight = CGFloat(300.0)
+
 struct ContentView: View {
     var body: some View {
         ZStack{
             Color(#colorLiteral(red: 0, green: 0.9103174806, blue: 1, alpha: 1))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Pizzas & Kebabs")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color(.secondarySystemGroupedBackground))
-                .padding()
+            VStack {
+                Image("KebabAndPizza")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: imageWith
+                           , height: imageHeight, alignment: .center)
+                Text("Pizzas & Kebabs")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(.secondarySystemGroupedBackground))
+                    .padding()
+            }
         }
     }
 }
