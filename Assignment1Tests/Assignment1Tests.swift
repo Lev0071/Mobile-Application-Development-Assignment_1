d9a15905ae09ea740aa9f1683d51caaa7f1467cd
Yasin Çakar <coder0071@outlook.com>
2021-03-18 21:28:25 +1000

    •• Add test cases to satisfy test driven development - file: Milestone1Tests.swift •

--- Assignment1Tests/Assignment1Tests.swift
+++ Assignment1Tests/Assignment1Tests.swift
@@ -29,5 +29,41 @@ class Assignment1Tests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    struct TestMileStone1 {
+    
+    static let story = """
+        
+        I love Kebab and Pizzas, not only because I am Mediterranean myself!
+        I love it because of its great taste despite its simplicity!
+        
+        For me a Kebab is an open pizza, or a portable pizza, and pizza is an open kebab when yo have time to put your foot up.
+        
+        Both have the 3 macronutrient groups...Protein, vegetables and Carbs and you can portion them when you prepare it 😋
+        """
+        
+        static let modelProperties = (foodName: "Kebabs And Pizzas", shortDescription: "A delicous yet simple mix of Proteins, Vegetables and Carbs", story: story)
+    
+        static let viewProperties = (imageName: "KebabAndPizza", backgroundColor: #colorLiteral(red: 0, green: 0.9103174806, blue: 1, alpha: 1), imageWith: CGFloat(350.0), imageHeight:CGFloat(350.0) )
+    
+    
+    }
+    
+    func testProject(){
+        
+        // Test Model Properties
+        XCTAssertEqual(TestMileStone1.modelProperties.foodName, Input.modelData.foodName)
+        XCTAssertEqual(TestMileStone1.modelProperties.shortDescription, Input.modelData.shortDescription)
+        XCTAssertEqual(TestMileStone1.modelProperties.story, Input.modelData.story)
+        
+        //Test ModelView Properties
+        XCTAssertEqual(TestMileStone1.viewProperties.imageName, Input.viewData.imageName)
+        XCTAssertEqual(TestMileStone1.viewProperties.backgroundColor, Input.viewData.backgroundColor)
+        XCTAssertEqual(TestMileStone1.viewProperties.imageWith, Input.viewData.imageWith)
+        XCTAssertEqual(TestMileStone1.viewProperties.imageHeight, Input.viewData.imageHeight)
+        
+    }
+    
+    
 
 }
